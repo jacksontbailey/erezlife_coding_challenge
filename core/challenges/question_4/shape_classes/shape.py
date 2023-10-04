994da0e647c4a60ce8a038f493e4a5a311f6287a
Jackson Bailey <Bailey95jackson@gmail.com>
2023-10-04 07:11:37 -0400

    Created 'Shape' class as the base class for all other shape classes.

---
+++ core/challenges/question_4/shape_classes/shape.py
@@ -0,0 +1,10 @@
+class Shape:
+    def __init__(self, name, side_length):
+        self.name = name
+        self.side_length = float(side_length)
+    
+    def calculate_perimeter(self):
+        pass
+    
+    def calculate_area(self):
+        pass
