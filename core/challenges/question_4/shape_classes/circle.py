93041d034d2d5fb3f32edfc7a8e83b02e4f6745c
Jackson Bailey <Bailey95jackson@gmail.com>
2023-10-04 07:16:03 -0400

    Create Circle class

---
+++ core/challenges/question_4/shape_classes/circle.py
@@ -0,0 +1,12 @@
+import math
+from shape_classes.shape import Shape
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__('circle', radius)
+    
+    def calculate_perimeter(self):
+        return 2 * math.pi * self.side_length
+    
+    def calculate_area(self):
+        return math.pi * self.side_length ** 2
