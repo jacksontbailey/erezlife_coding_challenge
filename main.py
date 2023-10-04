9fe74ac5e376bfb422c2e0793d9eae8a326c09c4
Jackson Bailey <Bailey95jackson@gmail.com>
2023-10-03 21:30:45 -0400

    Added 'run_question_1' function to control the whole process for question_1.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from core.config.database import DB_SETTING
-from core.challenges.question_1.student_applications import get_student_applications
+from core.challenges.question_1.run_question_1 import run_question_1
 
 if __name__ == "__main__":
     DB_SETTING.delete_tables()
-    DB_SETTING.create_tables()
-    DB_SETTING.add_dummy_data()
-    all_applications = get_student_applications()
+    all_applications = run_question_1()
     print(all_applications)
