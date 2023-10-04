19eb39bbab2320207d6d86df9f131bd05f917bab
Jackson Bailey <Bailey95jackson@gmail.com>
2023-10-04 07:14:27 -0400

    Create Triangle Class

---
+++ core/challenges/question_4/shape_classes/triangle.py
@@ -0,0 +1,12 @@
+import math
+from shape_classes.shape import Shape
+
+class Triangle(Shape):
+    def __init__(self, side_length):
+        super().__init__('triangle', side_length)
+    
+    def calculate_perimeter(self):
+        return 3 * self.side_length
+    
+    def calculate_area(self):
+        return 0.5 * self.side_length * math.sqrt(3)
